4a602fa6782b104cced1c9e5102feefac98d4d64
jonathan <johnkord@gmail.com>
2024-08-06 19:24:19 -0500

    dos

---
+++ microservice-student/src/main/java/com/microservice/student/entity/Student.java
@@ -0,0 +1,29 @@
+package com.microservice.student.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@Entity
+@Table(name = "students")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(name = "last_name")
+    private String lastName;
+    private String email;
+
+    @Column(name = "course_id")
+    private Long courseID;
+}
